1537d92d89c494abf9b73aa584f23f34496e3e85
fmarinoa <franco.edson.18014@gmail.com>
2025-07-29 17:32:19 -0500

    fix: open address

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,15 @@ import { Logger } from './helpers/logger';
 
 dotenv.config();
 const server = Fastify();
+const port: number = Number(process.env.PORT) || 3000;
 
 async function start(): Promise<void> {
   await connectToMongo();
 
   try {
     await registerRoutes(server);
-    await server.listen({ port: 3000 });
-    Logger.info('✅ Server running on http://localhost:3000');
+    const address = await server.listen({ port, host: '0.0.0.0' });
+    Logger.info(`✅ Server running on ${address}`);
   } catch (error) {
     Logger.error('❌ Error starting server:', error);
     process.exit(1);
